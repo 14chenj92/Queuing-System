e08fde3ffce54f18b94c95900699836f21251df7
J <09.chen@gmail.com>
2025-04-24 21:42:31 -0700

    Register Form Temporary Fix

--- script.js
+++ script.js
@@ -6,17 +6,17 @@ function sendMail(event) {
 
     let verificationCode = Math.floor(100000 + Math.random() * 900000); 
     let email = document.getElementById("email").value;
-    let firstName = document.getElementById("firstName").value;
-    let lastName = document.getElementById("lastName").value;
-    let now = new Date();
-    let currentDate = now.toLocaleString();
+    // let firstName = document.getElementById("firstName").value;
+    // let lastName = document.getElementById("lastName").value;
+    // let now = new Date();
+    // let currentDate = now.toLocaleString();
 
     let params = {
         email: email,
         code: verificationCode,
-        firstName: firstName,
-        lastName: lastName,
-        date: currentDate
+        // firstName: firstName,
+        // lastName: lastName,
+        // date: currentDate
     };
 
     emailjs.send("service_cuab1yr", "template_sidhjme", params)
